b0e00eb19539099563bdc1abfdd686f0e2d719d8
Jesus Zarate <chuy@goskip.com>
2018-10-01 22:44:23 -0600

    Added heroku url

--- rocketLeagueSite/Rankade/rankade.py
+++ rocketLeagueSite/Rankade/rankade.py
@@ -30,9 +30,14 @@ class Rankade:
 
             chrome_options = Options()
             chrome_options.binary_location = os.environ["GOOGLE_CHROME_BIN"]
-            chrome_options.add_argument('--disable-gpu')
-            chrome_options.add_argument("--disable-dev-shm-usage")
+            # chrome_options.add_argument('--disable-gpu')
+            # chrome_options.add_argument("--disable-dev-shm-usage")
+            # chrome_options.add_argument('--no-sandbox')
+
+            chrome_options = Options()
+            chrome_options.add_argument('--headless')
             chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-dev-shm-usage')
             self.driver = webdriver.Chrome(executable_path=os.environ["CHROMEDRIVER_PATH"], chrome_options=chrome_options)
 
         else:
